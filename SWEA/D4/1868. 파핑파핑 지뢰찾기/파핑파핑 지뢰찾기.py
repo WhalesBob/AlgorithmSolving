3ad2d2e1896aaca03514315c6ae8d72623a72ae5
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-07-02 10:31:36 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 863 ms, Memory: 107,308 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.py
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.py
@@ -1,11 +1,11 @@
 from collections import deque
-
+ 
 case = int(input())
 direct = [[-1,-1],[0,-1],[1,-1], [-1,0],[1,0],[-1,1],[0,1],[1,1]]
-
+ 
 def can_go(matrix, x, y):
     return 0 <= x < len(matrix[0]) and 0 <= y < len(matrix)
-
+ 
 def all_clear(matrix, x, y):
     for i in range(8):
         next_x = x + direct[i][0]
@@ -13,7 +13,7 @@ def all_clear(matrix, x, y):
         if can_go(matrix, next_x, next_y) and matrix[next_y][next_x] == '*':
             return False
     return matrix[y][x] != '*'
-
+ 
 def clear_others(matrix, x, y):
     global non_zero_count
     matrix[y][x] = '0'
@@ -29,7 +29,7 @@ def clear_others(matrix, x, y):
                     queue.append([next_x, next_y])
                 else:
                     non_zero_count -= 1
-
+ 
 for c in range(case):
     n = int(input())
     matrix = []
@@ -38,7 +38,7 @@ for c in range(case):
     non_zero_count = 0
     for i in range(n):
         matrix.append(list(map(str, input())))
-
+ 
     for y in range(n):
         for x in range(n):
             if all_clear(matrix, x, y):
@@ -47,7 +47,7 @@ for c in range(case):
                 non_zero_count += 1
             else:
                 pass
-
+ 
     answer = 0
     while len(zero_point) != 0:
         x,y = zero_point.popleft()
@@ -56,6 +56,6 @@ for c in range(case):
         else:
             answer += 1
             clear_others(matrix, x, y)
-
+ 
     answer += non_zero_count
     print("#{0} {1}".format(c+1,answer))
