63382f8ce0e0e4f48b0c13c6851630228a9beb9d
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-07-09 14:57:50 +0900

    [Silver I] Title: 트럭, Time: 160 ms, Memory: 116448 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -8,7 +8,7 @@ bridge_weight = 0
 bridge_queue = deque()
 time = 0
 
-while len(truck_queue) != 0:
+while True:
     time += 1
 
     for element in bridge_queue:
@@ -23,13 +23,7 @@ while len(truck_queue) != 0:
         bridge_queue.append([1, new_weight])
         bridge_weight += new_weight
 
-while len(bridge_queue) > 0:
-    time += 1
-    for element in bridge_queue:
-        element[0] += 1
-
-    while len(bridge_queue) > 0 and bridge_queue[0][0] > w:
-        bridge_weight -= bridge_queue[0][1]
-        bridge_queue.popleft()
+    if len(truck_queue) == 0 and len(bridge_queue) == 0:
+        break
 
 print(time)
