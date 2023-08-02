4a4f1da43ad8a88d77b9295d10f810e6cf2a43de
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-08-02 16:24:13 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 12824 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,37 +1,33 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] arr = new int[9];
-		for(int i = 0; i < 9; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] nine = new int[9];
+		int sum = 0;
+		for(int i = 0 ; i < 9;i++) {
+			nine[i] = sc.nextInt();
+			sum+=nine[i];
 		}
-		combination(arr, new boolean[9], 0, 9, 7);
-	}
-	static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
-		if(r == 0) {
-			int sum = 0;
-			for(int i = 0; i < 9; i++) {
-				if(visited[i]) {
-					sum += arr[i];
+		int test = sum;
+		for(int i = 0 ; i < 9 ; i ++) {
+			boolean finded=false;
+			test = sum;
+			test -= nine[i];
+			for(int j = i+1 ; j < 9;j++) {
+				test -= nine[j];
+				if(test == 100)	{
+					nine[i]=-1;
+					nine[j]=-1;
+					finded=true;
 				}
+				test += nine[j];
 			}
-			if(sum == 100) {
-				for(int i = 0; i < 9; i++) {
-					if(visited[i]) {
-						System.out.println(arr[i]);
-					}
-				}
-				System.exit(0);
-			}
-			return;
+			if(finded==true) break;
 		}
-		
-		for(int i = start; i < n; i++) {
-			visited[i] = true;
-			combination(arr, visited, i + 1, n, r - 1);
-			visited[i] = false;
+		for(int i = 0 ; i < 9 ; i ++) {
+			if(nine[i]==-1) continue;
+			else System.out.println(nine[i]);
 		}
 	}
 }
