8d80801ba0403e668d557ede79dcc32d33e1d9d2
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-08-04 13:46:38 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 51476 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,16 +1,28 @@
-import java.util.*;
-import java.util.stream.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        ArrayDeque<Integer> arrayDeque = Stream.iterate(1, x -> x + 1).limit(n).collect(Collectors.toCollection(ArrayDeque::new));
-        while(arrayDeque.size() != 1){
-            arrayDeque.removeFirst();
-            arrayDeque.addLast(arrayDeque.removeFirst());
-        }
-        System.out.println(arrayDeque.removeFirst());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Queue<Integer> q = new LinkedList<>();
+		int a = Integer.parseInt(br.readLine());
+		for(int i =0;i<a;i++) {
+			q.add(i+1);
+		}// 큐에 다 집어 넣기
+		while(q.size()>1) {
+			q.poll();
+			int b=q.peek();
+			q.poll();
+			q.add(b);
+		}
+		System.out.println(q.peek());
+		bw.flush();
+		bw.close();
+	}
 }
