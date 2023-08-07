f7db7e31c6dfecdebc778fbd7c205041eab69e0f
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-08-07 13:14:28 +0900

    [level 2] Title: 삼각 달팽이, Time: 55.12 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,35 +1,44 @@
-class Solution {
-    public int[] solution(int n) {
-        int[] array = new int[(n*(n+1))/2];
-        int turn_count = 0;
-        int value = 0;
-        int index = 0;
-        int add_value = 1;
+import java.util.*;
 
-        while(turn_count < n){
-            if(turn_count % 3 == 0){
-                for(int i = 0; i < n - turn_count; i++){
-                    index += value;
-                    array[index] = add_value;
-                    add_value++;
-                    value++;
-                }
-            }else if(turn_count % 3 == 1){
-                for(int i = 0; i < n - turn_count; i++){
-                    index += 1;
-                    array[index] = add_value;
-                    add_value++;
-                }
-            }else{
-                for(int i = 0; i < n - turn_count; i++){
-                    index -= value;
-                    array[index] = add_value;
-                    add_value++;
-                    value--;
-                }
-            }
-            turn_count++;
+class Solution {
+    static List<List<Integer>> direction = Arrays.asList(Arrays.asList(0,1), Arrays.asList(1,0), Arrays.asList(-1,-1)); 
+    public int[] solution(int n) {
+        int[][] matrix = new int[n][n];
+		int[] array = new int[(n*(n+1))/2];
+		
+        int turnCount = 0;
+        int index = 0;
+        int addValue = 1;
+        int x = 0, y = -1;
+        
+        while(true) {
+        	int nextX = x + direction.get(index).get(0);
+        	int nextY = y + direction.get(index).get(1);
+        	
+        	if(canGo(nextX, nextY, matrix) && matrix[nextY][nextX] == 0) {
+        		turnCount = 0;
+        		matrix[nextY][nextX] = addValue++;
+        		x = nextX;
+        		y = nextY;
+        		
+        	}else {
+        		if(turnCount == 3) {
+        			break;
+        		}
+        		turnCount++;
+        		index = (index + 1) % 3;
+        	}
+        }
+        
+        index = 0;
+        for(y = 0; y < matrix.length; y++) {
+        	for(x = 0; x <= y; x++) {
+        		array[index++] = matrix[y][x];
+        	}
         }
         return array;
-    }
+	}
+	static boolean canGo(int x, int y, int[][] matrix) {
+		return (0 <= x && x < matrix[0].length) && (0 <= y && y < matrix.length);
+	}
 }
