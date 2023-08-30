601dfdf4093d81ec2cff80a4437870c35553449c
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-08-30 16:39:40 +0900

    [Silver V] Title: 다리 놓기, Time: 196 ms, Memory: 17432 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,25 +1,44 @@
-import java.math.BigInteger;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int count = Integer.parseInt(scanner.nextLine());
-        for(int i = 0; i < count; i++){
-            String[] str = scanner.nextLine().split(" ");
-            int r = Integer.parseInt(str[0]);
-            int n = Integer.parseInt(str[1]);
 
-            BigInteger a = multiply(n-r+1,n);
-            BigInteger b = multiply(1,r);
-            System.out.println(a.divide(b));
-        }
-    }
-    static BigInteger multiply(int n, int m){
-        BigInteger answer = BigInteger.ONE;
-        for(int i = n; i <= m; i++){
-            answer = answer.multiply(BigInteger.valueOf(i));
-        }
-        return answer;
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int s = sc.nextInt()+1;
+			int e = sc.nextInt()+1;
+//			System.out.println(s+" "+e);
+			int[][] arr = new int[s][e];
+			for(int i=0;i<e;i++) {
+				arr[1][i]=i;
+			}
+//			for(int i=0;i<s;i++) {
+//				for(int j=0;j<e;j++) {
+//					System.out.print(arr[i][j]+ " ");
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+			for (int i = 2; i < s; i++) {
+				for (int j = 1; j < e; j++) {
+//					System.out.println(i+" "+j);
+					if (i == j)
+						arr[i][j] = 1;
+					else if(i<j) {
+						arr[i][j]=arr[i-1][j-1]+arr[i][j-1];
+					}
+				}
+			}
+//			for(int i=0;i<s;i++) {
+//				for(int j=0;j<e;j++) {
+//					System.out.print(arr[i][j]+ " ");
+//				}
+//				System.out.println();
+//			}
+			System.out.println(arr[s-1][e-1]);
+		}
+	}
+
 }
