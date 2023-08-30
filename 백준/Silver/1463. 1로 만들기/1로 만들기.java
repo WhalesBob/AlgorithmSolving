232ad0d2e4ee998957586324809d60576328aa1f
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-08-30 15:25:34 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 16904 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,28 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int[] dp = new int[n+1];
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(1);
-		
-		while(!queue.isEmpty() && dp[n] == 0) {
-			int element = queue.remove();
-			if(element + 1 <= n && (dp[element + 1] > element + 1 || dp[element + 1] == 0)) {
-				dp[element + 1] = dp[element] + 1;
-				queue.add(element + 1);
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int[] lst=new int[N+1];
+		lst[1]=0;
+		for(int i=2;i<N+1;i++) {
+			lst[i]=lst[i-1]+1;
+			
+			if(i%2==0) {
+				lst[i]=Math.min(lst[i], lst[i/2]+1);
 			}
-			if(element * 2 <= n && (dp[element * 2] > element + 1 || dp[element * 2] == 0)) {
-				dp[element * 2] = dp[element] + 1;
-				queue.add(element * 2);
-			} 
-			if(element * 3 <= n && (dp[element * 3] > element + 1 || dp[element * 3] == 0)) {
-				dp[element * 3] = dp[element] + 1;
-				queue.add(element * 3);
+			if(i%3==0) {
+				lst[i]=Math.min(lst[i], lst[i/3]+1);
 			}
 		}
-		System.out.println(dp[n]);
+		System.out.println(lst[N]);
 	}
+
 }
