8fe8eb8682d5ad960413b87b1f4c6305290eb4a0
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-09-25 08:40:56 +0900

    [Silver III] Title: N과 M (1), Time: 508 ms, Memory: 74784 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,43 @@
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static boolean[] check;
-	static String[] ans;
-	static int cnt=0;
-	public static void backTracking(int a,int b) throws IOException{
-		if(b==0) {
-			for(String s : ans) {
-				bw.write(s);
-				if(s==ans[cnt-1]) bw.write('\n');
-				else bw.write(' ');
-			}
-			return;
-		}
-		for(int i=0;i<a;i++) {
-			if(!check[i]) {
-				ans[cnt++]=Integer.toString(i+1);
-				check[i]=true;
-				backTracking(a,b-1);
-				cnt--;
-				check[i]=false;
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException{
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		check =new boolean[a];
-		ans = new String[b];
-		backTracking(a,b);
-		bw.flush();
-		bw.close();
-	}
+    static BufferedWriter bw;
+    static int n;
+    static int maxDepth;
+    static StringJoiner joiner;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = sc.nextInt();
+        maxDepth = sc.nextInt();
+        joiner = new StringJoiner(" ");
+
+        for(int i = 1; i <= n; i++){
+            makePermutation(1, Arrays.asList(i));
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    static void makePermutation(int depth, List<Integer> numberList) throws IOException {
+        if(depth == maxDepth){
+            for (Integer number : numberList) {
+                joiner.add(Integer.toString(number));
+            }
+            bw.write(joiner.toString() + "\n");
+            joiner = new StringJoiner(" ");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(!numberList.contains(i)){
+                ArrayList<Integer> newList = new ArrayList<>(numberList);
+                newList.add(i);
+                makePermutation(depth+1, newList);
+            }
+        }
+    }
 }
