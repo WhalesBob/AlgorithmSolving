6a958583016d9577c18b7dad26496d583dd817ac
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-10-05 15:55:21 +0900

    [Gold IV] Title: 스도쿠, Time: 596 ms, Memory: 19036 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,97 +1,124 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-	static int[][] matrix;
-	BufferedWriter bw;
-	public static void main(String[] args) throws IOException {
+	
+	static int[][] map;
+	static List<int[]> list;
+	static int length;
+	static boolean first;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int[][] box = makeBoxMatrix(br);
-		getCompleteMatrix(0, 0, box);
-	}
-	static void printAllMatrix() {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		// 0인 숫자에 하나씩 다 1부터 9까지 다 넣어봄
+		// 검사(해당 행과 열, 3x3)해서 안되면 버리고 이를 반복 (재귀)
 		
-		try {
-			for(int y = 0; y < 9; y++) {
-				for(int x = 0; x < 9; x++) {
-					bw.write(Integer.toString(matrix[y][x]));
-				}
-				bw.write("\n");
-			}
-			bw.flush();
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	static void getCompleteMatrix(int currentY, int currentX, int[][] box) {
-		int x = currentX, y = currentY;
+		map = new int[9][9];
+		list = new ArrayList<>();
 		
-		while(x < 9 && y < 9) {
-			if(matrix[y][x] == 0) {
-				for(int n = 1; n <= 9; n++) {
-					matrix[y][x] = n;
-					
-					if(isAllOK(x, y, box, n)) {
-						checkVisited(box, x, y, n);
-						getCompleteMatrix(y, x, box);
-						checkNotVisited(box, x, y, n);
-					}
-				}
-				matrix[y][x] = 0;
-				return;
-			}else {
-				if(x == 8) {
-					y++;
-					x = 0;
-				}else {
-					x++;
-				}
+		for(int i = 0; i < 9; i++) {
+			String temp = br.readLine();
+			for(int j = 0; j < 9; j++) {
+				map[i][j] = temp.charAt(j) - '0';
 			}
 		}
 		
-		if(x >= 9 || y >= 9) {
-			printAllMatrix();
-			System.exit(0);
+		DFS(0);
+		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println("");
 		}
-	}
-	static void checkVisited(int[][] box, int x, int y, int value) {
-		matrix[y][9] |= (1 << value);
-		matrix[9][x] |= (1 << value);
 		
-		box[y / 3][x / 3] |= (1 << value);
-	}
-	static void checkNotVisited(int[][] box, int x, int y, int value) {
-		matrix[y][9] &= ~(1 << value);
-		matrix[9][x] &= ~(1 << value);
 		
-		box[y / 3][x / 3] &= ~(1 << value);
-	}
-	static boolean isAllOK(int x, int y, int[][] box, int value) {
-		return isXOK(x, y, value) && isYOK(x, y, value) && isBoxOK(x, y, box, value);
-	}
-	static boolean isXOK(int x, int y, int value) {
-		return (matrix[y][9] & (1 << value)) == 0;
-	}
-	static boolean isYOK(int x, int y, int value) {
-		return (matrix[9][x] & (1 << value)) == 0;
-	}
-	static boolean isBoxOK(int x, int y, int[][] box, int value) {
-		return (box[y / 3][x / 3] & (1 << value)) == 0;
 	}
 	
-	static int[][] makeBoxMatrix(BufferedReader br) throws IOException{
-		matrix = new int[10][10];
-		int[][] box = new int[3][3];
-		for(int y = 0; y < 9; y++) {
-			char[] input = br.readLine().trim().toCharArray();
-			for(int x = 0; x < 9; x++) {
-				matrix[y][x] = input[x] - '0';
-				matrix[y][9] |= (1 << matrix[y][x]);
-				matrix[9][x] |= (1 << matrix[y][x]);
-				box[y / 3][x / 3] |= (1 << matrix[y][x]);
+	static void DFS(int cnt) {
+		if(cnt == 81) {
+			first = true;
+			return;
+		}
+		
+		int r = cnt / 9;
+		int c = cnt % 9;
+		
+		if(map[r][c] != 0) {
+			DFS(cnt +1);
+		}
+		
+		else {
+			for(int i = 1; i < 10; i++) {
+				if(check(r,c,i)) {
+					map[r][c] = i;
+					DFS(cnt+1);
+					if(first) {
+						return;
+					}
+					map[r][c] = 0;
+				}
 			}
 		}
-		return box;
+		
+	}
+	
+//	static void DFS(int cnt) {
+//		
+//		if(cnt == length) {
+//			first = true;
+//			return;
+//		}
+//		
+//		for(int j = 1; j < 10; j++) {
+//			
+//			int r = list.get(cnt)[0];
+//			int c = list.get(cnt)[1];
+//			System.out.println(r + " " + c);
+//			if(check(r,c,j)) {
+//				map[r][c] = j;
+//				
+//				DFS(cnt + 1);
+//				if(first) {
+//					return;
+//				}
+//				map[r][c] = 0;
+//			}
+//		
+//		}
+//		
+//	}
+	
+	static boolean check(int r, int c, int val) {
+		
+		for(int i = 0; i < 9; i++) {
+			if(map[r][i] == val) {
+				return false;
+			}
+			
+			if(map[i][c] == val) {
+				return false;
+			}
+		}
+		
+		int sr = (r/3) * 3;
+		int sc = c - (c%3);
+		
+		// c - (c%3)
+		// 0,0 / 3,0 / 6,0
+		// 3,0 / 3,3 / 3,6
+		// 6,0 / 6,3 / 6,6
+
+		for(int x = sr; x < sr+3; x++) {
+			for (int y = sc; y < sc+3; y++) {
+				if(map[x][y] == val) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
