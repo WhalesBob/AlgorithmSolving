c4f828ba421ba321db7868d00ee979ac8beaae7e
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-10-10 17:26:52 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 628 ms, Memory: 159832 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,121 +1,209 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static List<Point> conditioner;
-    static Queue<Integer> queue;
-    static List<List<Integer>> direction = Arrays.asList(Arrays.asList(1,0),
-            Arrays.asList(0,1), Arrays.asList(-1,0), Arrays.asList(0,-1));
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        conditioner = new ArrayList<>();
-        queue = new ArrayDeque<>();
 
-        int height = Integer.parseInt(st.nextToken());
-        int width = Integer.parseInt(st.nextToken());
-        int time = Integer.parseInt(st.nextToken());
+	static class Data {
+		int r, c, center, val;
+		boolean[] dir; // 1: 상 - 2: 하 - 3: 좌 - 4: 우
 
-        int[][] matrix = makeMatrix(br,height, width);
+		public Data(int r, int c, int center) {
+			this.r = r;
+			this.c = c;
+			this.center = center;
+			dir = new boolean[4];
+		}
 
-        for(int i = 0; i < time; i++){
-            spread(matrix);
-            Point up = conditioner.get(0);
-            Point down = conditioner.get(1);
-            rotate(matrix, 0,0, matrix[0].length - 1, up.y, true);
-            rotate(matrix, 0, down.y, matrix[0].length - 1, matrix.length - 1, false);
+	}
+
+	static int R, C, T;
+	static int[][] map;
+	static int airpos1, airpos2;
+	static int[] dr = { -1, 1, 0, 0 }; // 상 하 좌 우
+	static int[] dc = { 0, 0, -1, 1 };
+	static List<Data> list;
+
+	public static void main(String[] args) throws Exception {
+		// 미세먼지의 확산은 동시에 진행된다.
+		// 미세먼지는 공기청정기가 있는 곳엔 확산되지 않는다.
+		// 확산되는 양은 A(r,c) / 5이다
+		// 확산되고 남는 양은 A(r,c) - A(r,c) / 5 * (확산된 방향의 개수)
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+
+		map = new int[R][C];
+		list = new ArrayList<>();
+
+		for (int i = 0; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				// 미세먼지 좌표 얻어오기
+				if (map[i][j] > 0) {
+					list.add(new Data(i, j, map[i][j]));
+				}
+			}
+		}
+
+		for(int i = 0; i < R; i++) {
+			// 공기청정기 좌표 얻어오기
+			if (map[i][0] == -1) {
+				airpos1 = i;
+				airpos2 = i + 1;
+				break;
+			}
+		}
+		
+		// T초만큼 진행
+		for (int time = 0; time < T; time++) {
+			
+			if(list.size() == 0) {
+				for(int i = 0; i < R; i++) {
+					for(int j = 0; j < C; j++) {
+						if(map[i][j] > 0) {
+							list.add(new Data(i, j, map[i][j]));
+						}
+					}
+				}
+			}
+			
+			// 미세먼지 동시 확산
+			mise();
+			// 공기 청정기 작동
+			clean();
+		}
+		int result = getMise();
+		System.out.println(result + 2);
+	}
+
+	
+	static void mise() {
+		// 미세먼지를 어떻게 동시에 확산 시킬까
+		// 일단 4방위 탐색을 해야함. 4방위 탐색을 해서 퍼질 수 있는 공간의 갯수, 좌표를 구해야함
+		// 그러나 이 정보를 어디에다가 저장을 해야할까? 차라리 그냥 list를 하나 더 만들어서
+		// 자료형을 class로 저장하는 것이 좋을 것 같음.
+		// class에 상 하 좌 우의 정보를 담아두기
+
+		// 일단 list에 있는 미세먼지 하나씩 꺼내기
+		for (int i = 0; i < list.size(); i++) {
+
+			int r = list.get(i).r;
+			int c = list.get(i).c;
+
+			// 꺼낸 미세먼지 4방위 탐색
+			int cnt = 0;
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+
+				if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
+					continue;
+				}
+
+				if (map[nr][nc] == -1) {
+					continue;
+				}
+
+				// 해당 방향 check;
+				list.get(i).dir[d] = true;
+				cnt++;
+			}
+
+			// 확산 되는 값 저장하기
+			list.get(i).val = list.get(i).center / 5;
+
+			// 확산되고 남는 값을 저장하기
+			list.get(i).center = list.get(i).center - (list.get(i).center / 5 * cnt);
+			map[r][c] = list.get(i).center;
+		}
+
+		// 확산되는 값들 더해주기
+		for (int i = 0; i < list.size(); i++) {
+
+			int r = list.get(i).r;
+			int c = list.get(i).c;
+
+			// 확산 값
+			int val = list.get(i).val;
+
+			for (int d = 0; d < 4; d++) {
+				if (list.get(i).dir[d]) {
+					int nr = r + dr[d];
+					int nc = c + dc[d];
+					map[nr][nc] += val;
+				}
+			}
+		}
+		
+		list.clear();
+	}
+
+	static void clean() {
+		int top = airpos1;
+		
+		for(int r = top - 1; r > 0; r--) {
+			map[r][0] = map[r - 1][0];
+		}
+		
+		for(int c = 0; c < C - 1; c++) {
+			map[0][c] = map[0][c + 1];
+		}
+		
+		for(int r = 0; r < top; r++) {
+			map[r][C - 1] = map[r + 1][C - 1];
+		}
+		
+		for(int c = C - 1; c > 1; c--) {
+			map[top][c] = map[top][c - 1];
+		}
+		
+		map[top][1] = 0;
+		
+		int bottom = airpos2;
+		 
+        for (int r = bottom + 1; r < R - 1; r++) {
+            map[r][0] = map[r + 1][0];
         }
+ 
+        for (int c = 0; c < C - 1; c++) {
+            map[R - 1][c] = map[R - 1][c + 1];
+        }
+ 
+        for (int r = R - 1; r > bottom; r--) {
+            map[r][C - 1] = map[r - 1][C - 1];
+        }
+ 
+        for (int c = C - 1; c > 1; c--) {
+            map[bottom][c] = map[bottom][c - 1];
+        }
+ 
+        map[bottom][1] = 0;
+	}
 
-        int count = 0;
-        for(int y = 0; y < matrix.length; y++){
-            for(int x = 0; x < matrix[0].length; x++){
-                if(matrix[y][x] != -1){
-                    count += matrix[y][x];
-                }
-            }
-        }
-        System.out.println(count);
-    }
-    static void rotate(int[][] matrix, int startX, int startY, int endX, int endY, boolean up){
-        queue.clear();
-        int dirIndex = 0;
-        int x = startX + 1,y = up ? endY : startY;
-        int addIndex = up ? -1 : 1;
-        queue.add(matrix[y][x]);
-        matrix[y][x] = 0;
-
-        while(true){
-            int newX = x + direction.get(dirIndex).get(0);
-            int newY = y + direction.get(dirIndex).get(1);
-
-            if(inBound(newX, newY, startX, startY, endX, endY)){
-                if(matrix[newY][newX] == -1){
-                    break;
-                }
-                queue.add(matrix[newY][newX]);
-                matrix[newY][newX] = queue.remove();
-                x = newX; y = newY;
-            }
-            else {
-                dirIndex = (dirIndex + addIndex + 4) % 4;
-            }
-        }
-    }
-    static int[][] makeMatrix(BufferedReader br, int height, int width) throws IOException{
-        int[][] matrix = new int[height][width];
-        for(int y = 0; y < height; y++){
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int x = 0; x < width; x++){
-                matrix[y][x] = Integer.parseInt(st.nextToken());
-                if(matrix[y][x] == -1){
-                    conditioner.add(new Point(x,y));
-                }
-            }
-        }
-        return matrix;
-    }
-    static void spread(int[][] matrix){
-        int[][] spreadMatrix = new int[matrix.length][matrix[0].length];
-
-        for(int y = 0; y < matrix.length; y++){
-            for(int x = 0; x < matrix[0].length; x++){
-                if(matrix[y][x] > 0){
-                    spreadOne(matrix, spreadMatrix, x,y);
-                }
-            }
-        }
-
-        for(int y = 0; y < matrix.length; y++){
-            for(int x = 0; x < matrix[0].length; x++){
-                matrix[y][x] += spreadMatrix[y][x];
-            }
-        }
-    }
-    static void spreadOne(int[][] matrix, int[][] resultMatrix, int x, int y){
-        int origin = matrix[y][x];
-        for(int i = 0; i < 4; i++){
-            int newX = x + direction.get(i).get(0);
-            int newY = y + direction.get(i).get(1);
-            if(canGo(newX, newY, matrix) && matrix[newY][newX] != -1){
-                int value = origin / 5;
-                resultMatrix[newY][newX] += value;
-                matrix[y][x] -= value;
-            }
-        }
-    }
-    static boolean canGo(int x, int y, int[][] matrix){
-        return (0 <= x && x < matrix[0].length) && (0 <= y && y < matrix.length);
-    }
-    static boolean inBound(int x, int y, int startX, int startY, int endX, int endY){
-        return (startX <= x && x <= endX) && (startY <= y && y <= endY);
-    }
-    static class Point{
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static int getMise() {
+		int sum = 0;
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+					sum += map[i][j];
+			}
+		}
+		return sum;
+	}
+	
+	static void debug() {
+		for(int i = 0; i < R; i++) {
+			for(int j = 0; j < C; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+	}
 }
